09b10069bc573f879a61bc0744a31d0e7a26b52c
刘顺 <liushun@u51.com>
2016-06-13 13:10:12 +0800

    pojo module

---
+++ pojo-4-use/src/main/java/com/lsgggg123/springboot/pojo/Pojo.java
@@ -0,0 +1,34 @@
+package com.lsgggg123.springboot.pojo;
+
+/**
+ * Created by liushun on 16/6/13.
+ */
+public class Pojo {
+    private Integer id;
+    private String name;
+    private String tag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
